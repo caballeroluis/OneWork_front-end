af234134d175280aed2f5ef85865c4841a79997f
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2022-02-18 20:33:41 +0100

    ng-new: hidden login buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   smallScreen: boolean = window.innerWidth < 480;
 
   constructor(
-    private stateSS: StateStoreService
+    public stateSS: StateStoreService
   ) { }
 
   printState() {

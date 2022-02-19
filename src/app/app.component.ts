ee58807671a4908e961843fee7544fb1c5100ce7
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2022-02-19 15:25:24 +0100

    ng-new: Responsive observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ import { environment } from '@env/environment';
 export class AppComponent {
   
   env = environment;
-  smallScreen: boolean = window.innerWidth < 480;
 
   constructor(
     public stateSS: StateStoreService
@@ -21,6 +20,7 @@ export class AppComponent {
     console.log('📂 Users data', this.stateSS.users);
     console.log('📂 Offers data', this.stateSS.offers);
     console.log('👀 Session observers', this.stateSS.session$['source']);
+    console.log('👀 SmallScreen observers', this.stateSS.smallScreen$['source']);
     console.log('👀 Users observers', this.stateSS.users$['source']);
     console.log('👀 Offers observers', this.stateSS.offers$['source']);
   }
--- src/app/core/services/state/state-store.service.ts
+++ src/app/core/services/state/state-store.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Session } from '@sections/session/models';
 import { Offer, User } from '@shared/models';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, fromEvent, Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateStoreService {
+
+  private readonly _smallScreen = new BehaviorSubject<boolean>(window.innerWidth < 480);
+  public readonly smallScreen$ = this._smallScreen.asObservable();
 
   private readonly _session = new BehaviorSubject<Session>(new Session());
   public readonly session$ = this._session.asObservable();
@@ -21,7 +24,16 @@ export class StateStoreService {
     map(users => users.filter(user => user.role === 'worker'))
   );
 
-  constructor() { }
+  constructor() {
+    this.onInit();
+  }
+
+  get smallScreen(): boolean {
+    return this._smallScreen.getValue();
+  }
+  set smallScreen(val: boolean) {
+    this._smallScreen.next(val);
+  }
 
   get session(): Session {
     return this._session.getValue();
@@ -42,6 +54,18 @@ export class StateStoreService {
   }
   set users(val: User[]) {
     this._users.next(val);
+  }
+
+  onInit() {
+    this.initializeWindowWidthObserver();
+  }
+
+  initializeWindowWidthObserver() {
+    fromEvent(window, 'resize').pipe(
+      map((event: any) => event.target.innerWidth < 480)
+    ).subscribe(async (smallScreen: boolean) => {
+      this.smallScreen = smallScreen
+    });
   }
 
   clear() {
